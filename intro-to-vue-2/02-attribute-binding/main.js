bac5358210d95df28dd22e223176be8977e405d2
Andrew White <a.white@wdm.com.au>
2021-11-20 17:17:59 +1000

    Add completed lesson 2 code
    Code at end of lesson.

---
+++ intro-to-vue-2/02-attribute-binding/main.js
@@ -0,0 +1,9 @@
+// Create a new Vue instance and bind to the '#app' element.
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+  },
+})
