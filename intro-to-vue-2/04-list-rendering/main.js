7a27e77049eadb7d27f45e1ce9e4f5d9f5ed9360
Andrew White <a.white@wdm.com.au>
2021-11-20 17:45:25 +1000

    Add completed lesson 4 code

---
+++ intro-to-vue-2/04-list-rendering/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "Green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "Blue",
+      },
+    ],
+  },
+})
