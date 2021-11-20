1c79029791085ddf9bc75d63d305d865b985db0a
Andrew White <a.white@wdm.com.au>
2021-11-20 12:20:02 +1000

    Add completed challenge 1 code
    Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.

--- intro-to-vue-2/01-the-vue-instance/main.js
+++ intro-to-vue-2/01-the-vue-instance/main.js
@@ -1,7 +1,9 @@
 // Create a new Vue instance and bind to the '#app' element.
+
 var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
+    description: "A pair of warm, fuzzy socks",
   },
 });
