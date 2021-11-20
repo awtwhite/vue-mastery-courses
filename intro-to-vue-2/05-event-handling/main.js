3608342104d1de05f58cead43d52aa5e28522550
Andrew White <a.white@wdm.com.au>
2021-11-20 18:08:03 +1000

    Add completed lesson 5 code

---
+++ intro-to-vue-2/05-event-handling/main.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "Green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "Blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1
+    },
+    // The method below is written with the es6 shorthand. The method above is
+    // as the traditional way.
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+  },
+})
