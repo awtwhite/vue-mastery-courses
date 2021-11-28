a3f3811ea778c90a88d23e4eb414b9a84ef1db59
Andrew White <a.white@wdm.com.au>
2021-11-29 06:51:20 +1000

    Add feather icons and setup icon component

--- real-world-vue-2/real-world-vue/src/components/BaseIcon.vue
+++ real-world-vue-2/real-world-vue/src/components/BaseIcon.vue
@@ -1,12 +1,50 @@
 <template>
-  <div>
-    Icon
-  </div>
+  <!-- `v-html="svg"` will render what is returned from the computed property 'svg'
+  as the inner HTML of the element -->
+  <div class="icon-wrapper" v-html="svg"></div>
 </template>
 
 <script>
-export default {}
+import feather from 'feather-icons'
+
+export default {
+  props: {
+    name: String, // The name of the symbol we want to use.
+    width: {
+      type: [Number, String], // Type can be a number or a string.
+      default: 24
+    },
+    height: {
+      type: [Number, String], // Type can be a number or a string.
+      default: 24
+    }
+  },
+  computed: {
+    svg() {
+      return feather.icons[this.name].toSvg({
+        class: 'icon',
+        width: this.width,
+        height: this.height
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
 </style>
