8c0f2f063fe963d31c7a1d48b22f7de9532dc9dd
Andrew White <a.white@wdm.com.au>
2021-11-29 06:28:49 +1000

    Register a global component Registering a component globally means you can use inside other components without importing that component first.

--- real-world-vue-2/real-world-vue/src/main.js
+++ real-world-vue-2/real-world-vue/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue'
+
+// Globally register a component.
+// NOTE: Must be above the creation of the new Vue instance.
+Vue.component('BaseIcon', BaseIcon)
 
 Vue.config.productionTip = false
 
