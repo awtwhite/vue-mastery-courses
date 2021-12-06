e326dc739cdb2fcdf7e57f088656f93680ba071c
Andrew White <a.white@wdm.com.au>
2021-12-06 21:36:23 +1000

    Import 'mapState' to get store values as computed properties

--- mastering-vuex/src/views/EventCreate.vue
+++ mastering-vuex/src/views/EventCreate.vue
@@ -2,18 +2,21 @@
   <div>
     <h1>Create an Event, {{ userName }}</h1>
     <p> This event was created by {{ userName }}</p>
+    <p> User ID: {{ userId }}</p>
+    <p> Event categories: {{ categories }}</p>
   </div>
 </template>
 
 <script>
+  // See https://vuex.vuejs.org/guide/state.html#the-mapstate-helper
+  import { mapState } from 'vuex'
+
   export default {
-    computed: {
-      userName() {
-        return this.$store.state.user.name
-      },
-      userId() {
-        return this.$store.state.user.id
-      }
-    }
+    computed: mapState({
+      // arrow functions can make the code very succinct!
+      userName: state => state.user.name,
+      userId: state => state.user.id,
+      categories: state => state.categories
+    })
   }
 </script>
